3550d3607b2ac00b5a7f93126498681291489d4b
Jordan Kline <jordan.kline@gmail.com>
2018-02-13 17:54:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+
+    redirect to '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+
     erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
   end
 end
